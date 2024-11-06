be8342da43ef381044f8b6166d9ccb45ec4c3e18
Vladimir <bizin.vladimir01@gmail.com>
2024-11-06 16:07:59 +0300

    write counter '+', '-', and 'zero' functionality

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -9,11 +9,52 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var counts = 0 {
+        didSet {
+            counterLabel.text = "Значение счетчика: \(counts)"
+        }
+    }
+    
+    private var time: String {
+        Date().formatted(date: .numeric, time: .standard)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        increaseButton.tintColor = .red
+        decreaseButton.tintColor = .systemBlue
+        restartButton.tintColor = .systemGray
+        
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var counterLabel: UILabel!
+    
+    @IBOutlet weak var logText: UITextView!
+    
+    @IBOutlet weak var increaseButton: UIButton!
+    
+    @IBOutlet weak var decreaseButton: UIButton!
+    
+    @IBOutlet weak var restartButton: UIButton!
+    
+    @IBAction func increaseAction() {
+        counts += 1
+        logText.text += "\n\(time): значение изменено на +1"
+    }
+    
+    @IBAction func decreaseAction() {
+        if counts == 0 {
+            logText.text += "\n\(time): попытка уменьшить значение ниже 0"
+        } else {
+            counts -= 1
+            logText.text += "\n\(time): значение изменено на -1"
+        }
+    }
+    
+    @IBAction func restartAction() {
+        counts = 0
+        logText.text += "\n\(time): значение сброшено"
+    }
 }
 
